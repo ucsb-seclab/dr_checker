8bf5aa237cfb07df96559bec7ada8599c5d9965c
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2017-09-14 12:52:18 -0700

    Separate ResultItem in ResultItemHeader and ResultItemBody
    In order to have a better code organization I decided to split everthing I can in subcomponents.
    
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- visualizer/client/src/components/resultItem.jsx
+++ visualizer/client/src/components/resultItem.jsx
@@ -1,14 +1,9 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import Card, { CardContent, CardActions } from 'material-ui/Card';
-import Collapse from 'material-ui/transitions/Collapse';
-import Divider from 'material-ui/Divider';
-import Typography from 'material-ui/Typography';
-import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
-import IconButton from 'material-ui/IconButton';
+import Card from 'material-ui/Card';
 import { withStyles } from 'material-ui/styles';
+import ResultItemHeader from './resultItemHeader.jsx';
+import ResultItemBody from './resultItemBody.jsx';
 
 const styles = theme => ({
   card: {
@@ -16,21 +11,6 @@ const styles = theme => ({
     backgroundColor: theme.palette.grey[700],
     marginTop: 10,
     marginBottom: 10,
-  },
-  cardTitle: {
-    paddingLeft: 12,
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  flexGrow: {
-    flex: '1 1 auto',
   },
 });
 
@@ -49,6 +29,7 @@ class ResultItem extends React.Component {
 
   /**
    * Method that handle when the user clicks on the icon to expand the card
+   * It basically toggle the expansion attribute everytime the user clicks on the button
    */
   handleExpandClick = () => {
     this.setState({ expanded: !this.state.expanded });
@@ -58,52 +39,12 @@ class ResultItem extends React.Component {
     const classes = this.props.classes;
     return (
       <Card className={classes.card}>
-        <CardActions disableActionSpacing>
-          <Typography type="subheading" className={classes.cardTitle}>
-            {this.props.functionName}
-          </Typography>
-          <div className={classes.flexGrow} />
-          <IconButton
-            className={classnames(classes.expand, {
-              [classes.expandOpen]: this.state.expanded,
-            })}
-            onClick={this.handleExpandClick}
-            aria-expanded={this.state.expanded}
-            aria-label="Show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-        </CardActions>
-        <Collapse in={this.state.expanded} transitionDuration="auto" unmountOnExit>
-          <Divider />
-          <CardContent>
-            <Typography paragraph type="body2">
-                Method:
-            </Typography>
-            <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                minutes.
-            </Typography>
-            <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-                chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-                salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-                minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes and peppers, and
-                cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-                Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into
-                the rice, and cook again without stirring, until mussels have opened and rice is
-                just tender, 5 to 7 minutes more. (Discard any mussels that don’t open.)
-            </Typography>
-            <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
-          </CardContent>
-        </Collapse>
+        <ResultItemHeader
+          title={this.props.functionName}
+          expanded={this.state.expanded}
+          handleExpandClick={this.handleExpandClick}
+        />
+        <ResultItemBody expanded={this.state.expanded} />
       </Card>
     );
   }
---
+++ visualizer/client/src/components/resultItemHeader.jsx
@@ -0,0 +1,66 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { CardActions } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
+import IconButton from 'material-ui/IconButton';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+  cardTitle: {
+    paddingLeft: 12,
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  flexGrow: {
+    flex: '1 1 auto',
+  },
+});
+
+/**
+ * This class represent the header of the resultItem.
+ * It shows the name of the function which the result refer to and implements
+ * the button to expand/collapse the result.
+ */
+function ResultItemHeader(props) {
+  const classes = props.classes;
+
+  return (
+    <CardActions disableActionSpacing>
+      {/* Title: function name */}
+      <Typography type="subheading" className={classes.cardTitle}>
+        {props.title}
+      </Typography>
+      <div className={classes.flexGrow} />
+      {/* expand/collapse button */}
+      <IconButton
+        className={classnames(classes.expand, {
+          [classes.expandOpen]: props.expanded,
+        })}
+        onClick={props.handleExpandClick}
+        aria-expanded={props.expanded}
+        aria-label="Show more"
+      >
+        <ExpandMoreIcon />
+      </IconButton>
+    </CardActions>
+  );
+}
+
+ResultItemHeader.propTypes = {
+  classes: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+  expanded: PropTypes.bool.isRequired,
+  handleExpandClick: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(ResultItemHeader);
