13a7f9f641dea6408fbb8c81a6e86455c53b2f5a
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2017-09-14 14:14:29 -0700

    Add section components to resultItemBody
    The ody of the resultItem is now splitted into two sections: the first one shows the summary of the analysis relative to the selected function, while the second one shows the sourcecode highlighted accordingling.
    
    Both needs to be implemented... This is just the GUI skeleton.
    
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- visualizer/client/src/components/resultItemBody.jsx
+++ visualizer/client/src/components/resultItemBody.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { CardContent } from 'material-ui/Card';
 import Collapse from 'material-ui/transitions/Collapse';
 import Divider from 'material-ui/Divider';
-import Typography from 'material-ui/Typography';
+import SectionTitle from './sectionTitle.jsx';
 
 /**
  * This class represent the body of the resultItem.
@@ -12,43 +12,20 @@ import Typography from 'material-ui/Typography';
  * the highlighted source code, the summary of the analysis relative to this
  * this function, etc...
  */
-function ResultItemHeader(props) {
+function ResultItemBody(props) {
   return (
     <Collapse in={props.expanded} transitionDuration="auto" unmountOnExit>
       <Divider />
       <CardContent>
-        <Typography paragraph type="body2">
-                Method:
-        </Typography>
-        <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                minutes.
-        </Typography>
-        <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-                chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-                salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-                minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-        </Typography>
-        <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes and peppers, and
-                cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-                Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into
-                the rice, and cook again without stirring, until mussels have opened and rice is
-                just tender, 5 to 7 minutes more. (Discard any mussels that don’t open.)
-        </Typography>
-        <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
-        </Typography>
+        <SectionTitle title="Summary" />
+        <SectionTitle title="Source Code" />
       </CardContent>
     </Collapse>
   );
 }
 
-ResultItemHeader.propTypes = {
+ResultItemBody.propTypes = {
   expanded: PropTypes.bool.isRequired,
 };
 
-export default ResultItemHeader;
+export default ResultItemBody;
---
+++ visualizer/client/src/components/sectionTitle.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+  title: {
+    color: theme.palette.common.fullWhite,
+    borderBottom: `1px solid ${theme.palette.secondary[300]}`,
+    marginLeft: '40%',
+    marginRight: '40%',
+    fontFamily: 'monospace',
+  },
+});
+
+/**
+ * Simple class to set the style for the section title 
+ */
+function SectionTitle(props) {
+  const classes = props.classes;
+
+  return (
+    <Typography
+      type="subheading"
+      align="center"
+      className={classes.title}
+    >
+      {props.title.toUpperCase()}
+    </Typography>
+  );
+}
+
+SectionTitle.propTypes = {
+  classes: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default withStyles(styles)(SectionTitle);
