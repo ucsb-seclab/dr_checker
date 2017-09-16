676f3b7595534ad04180d8716390e41e3b0b0418
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2017-09-16 16:18:27 -0700

    Add route /result/analysis_name
    This route return the summary of the selected analysis and the source code analyzed.
    
    TODO: modify the JSON output of dr_checker before getting the sourcecode.
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- visualizer/server/app.py
+++ visualizer/server/app.py
@@ -17,7 +17,6 @@ def get_results():
     response = {"success" : True, "data" : []}
     # return error if path does not exist
     if not os.path.exists(app.config["RESULTS_DIR"]):
-        print app.config["RESULTS_DIR"]
         response["success"] = False
         response["msg"] = "Misconfigured results_dir"
         return jsonify(response)
@@ -25,6 +24,27 @@ def get_results():
     for filename in os.listdir(app.config["RESULTS_DIR"]):
         response["data"].append({"name" : os.path.splitext(filename)[0]})
     return jsonify(response)
+
+@app.route("/result/<string:filename>")
+def get_result(filename):
+    """
+    This route returns the details of the analysis relative to the filename specified
+    along with the source code analyzed
+    """
+    response = {"success": True, "data" : {}}
+    analysis_content_file_path = os.path.join(app.config["RESULTS_DIR"], filename + '.json')
+    print analysis_content_file_path
+    # returns error if filename does not exist
+    if not os.path.exists(analysis_content_file_path):
+        response["success"] = False
+        response["msg"] = "The requested file does not exist"
+        return jsonify(response)
+    # returns the analysis results and the seurce code of the analyzed file
+    response["data"]["summary"] = open(analysis_content_file_path, "r").readlines()
+    return jsonify(response)
+    
+
+
 
 if __name__ == "__main__":
     app.run()
