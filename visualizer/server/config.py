7d6ab7ba898870eb630d5e033687b309a6da595c
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2017-09-22 13:42:29 -0700

    Add simple readme for visualizer
    Add a readme file that explains how to install and run the visualizer.
    
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- visualizer/server/config.py
+++ visualizer/server/config.py
@@ -2,7 +2,7 @@
 List of all the settings of thge web server
 """
 
-DEBUG = True
+DEBUG = False
 
 # dr_checker's output fgolder
 RESULTS_DIR = "/Users/phate/Desktop/Seba_stuff/output_files/results"
