a88208b575b6400290e54e9b5ccf29fb52a45707
Aravind Machiry <machiry@cs.ucsb.edu>
2018-02-16 14:34:01 -0800

    Adding new deps

--- docker/main/install.sh
+++ docker/main/install.sh
@@ -7,3 +7,6 @@ cd gitrepo/helper_scripts
 python setup_drchecker.py -o /dr_checker/drcheckerdeps
 echo 'export LLVM_ROOT=/dr_checker/drcheckerdeps/llvm/build' >> ~/.bashrc
 echo 'export PATH=$LLVM_ROOT/bin:/dr_checker/drcheckerdeps/sparse:$PATH' >> ~/.bashrc
+source ~/.bashrc
+cd /dr_checker/gitrepo/llvm_analysis
+./build.sh
