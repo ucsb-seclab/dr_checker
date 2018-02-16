5a38318a1d06d9a2105776d4d56caa5556478df0
Aravind, Machiry <machiry@cs.ucsb.edu>
2018-02-16 14:43:13 -0800

    Adding docker install script

---
+++ docker/install_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get install -y docker.io
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -a -G docker $USER
