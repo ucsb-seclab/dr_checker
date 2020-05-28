103a732c92f5eb525ff797c4c88be9b73fb0f38a
Derrick McKee <derrick.mckee@gmail.com>
2020-05-28 14:10:36 -0400

    fixing setup script to use the new way of building LLVM

--- helper_scripts/setup_drchecker.py
+++ helper_scripts/setup_drchecker.py
@@ -34,17 +34,23 @@ def log_success(*args):
     for curr_a in args:
         log_str = log_str + " " + str(curr_a)
     print log_str
-    
 
-LLVM_GIT_HUB_BASE = "https://github.com/llvm-mirror/"
+
+def execute_command(cmd):
+    if os.system(cmd) != 0:
+        log_error("Command {} failed".format(cmd))
+        exit(1)
+
+LLVM_GIT_HUB_BASE = "https://github.com/llvm/llvm-project.git"
 SPARSE_URL = "git://git.kernel.org/pub/scm/devel/sparse/sparse.git"
 
 
 def setup_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-b', action='store', dest='target_branch', default='release_38',
-                        help='Branch (i.e. version) of the LLVM to setup. Default: release_38 e.g., release_38')
+    parser.add_argument('-b', action='store', dest='target_branch',
+                        default='release/3.8.x',
+                        help='Branch (i.e. version) of the LLVM to setup. Default: release/3.8.x')
 
     parser.add_argument('-o', action='store', dest='output_folder',
                         help='Folder where everything needs to be setup.')
@@ -62,50 +68,45 @@ def main():
     arg_parser = setup_args()
     parsed_args = arg_parser.parse_args()
     # step 1: Setup common dictionary
-    reps_to_setup = {'tools': ['clang'], 'projects': ['compiler-rt', 'libcxx', 'libcxxabi', 'openmp']}
+    reps_to_setup = ['clang', 'compiler-rt', 'libcxx', 'libcxxabi', 'openmp']
     if parsed_args.output_folder is None:
         usage()
     sparse_dir = os.path.join(parsed_args.output_folder, "sparse")
     base_output_dir = os.path.join(parsed_args.output_folder, "llvm")
     target_branch = parsed_args.target_branch
     log_info("Preparing setup in:", parsed_args.output_folder)
-    log_info("Cloning sparse")
     backup_dir = os.getcwd()
-    os.system('mkdir -p ' + sparse_dir)
-    os.system("git clone " + SPARSE_URL + " " + sparse_dir)
+    if not os.path.exists(sparse_dir):
+        log_info("Cloning sparse")
+        execute_command('mkdir -p ' + sparse_dir)
+        execute_command("git clone " + SPARSE_URL + " " + sparse_dir)
     os.chdir(sparse_dir)
-    os.system("make")
+    execute_command("make")
     os.chdir(backup_dir)
     log_success("Successfully built sparse.")
-    log_info("Cloning LLVM")
-    os.system('mkdir -p ' + str(base_output_dir))
-    git_clone_cmd = "git clone " + LLVM_GIT_HUB_BASE + "llvm" + " -b " + \
-                    str(target_branch) + " " + base_output_dir
-    log_info("Setting up llvm.")
-    os.system(git_clone_cmd)
+    if not os.path.exists(base_output_dir):
+        log_info("Cloning LLVM")
+        execute_command('mkdir -p ' + str(base_output_dir))
+        git_clone_cmd = "git clone " + LLVM_GIT_HUB_BASE + " -b " + \
+                        str(target_branch) + " " + base_output_dir
+        log_info("Setting up llvm.")
+        execute_command(git_clone_cmd)
 
-    for curr_folder in reps_to_setup:
-        rep_folder = os.path.join(base_output_dir, curr_folder)
-        for curr_rep in reps_to_setup[curr_folder]:
-            dst_folder = os.path.join(rep_folder, curr_rep)
-            git_clone_cmd = "git clone " + LLVM_GIT_HUB_BASE + str(curr_rep) + " -b " + \
-                            str(target_branch) + " " + dst_folder
-            log_info("Setting up ", curr_rep, " repo.")
-            os.system(git_clone_cmd)
+        log_success("Cloned all the repositories.")
 
-    log_success("Cloned all the repositories.")
     build_dir = os.path.join(base_output_dir, "build")
     log_info("Trying to build in ", build_dir)
-    os.system('mkdir -p ' + build_dir)
+    if not os.path.exists(build_dir):
+        execute_command('mkdir -p ' + build_dir)
     os.chdir(build_dir)
-    os.system('cmake ..')
+    execute_command('cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS=\'{}\' ../llvm'.format(";".join(reps_to_setup)))
     multi_proc_count = cpu_count()
     if multi_proc_count > 0:
         log_info("Building in multiprocessing mode on ", multi_proc_count, " cores.")
-        os.system('make -j' + str(multi_proc_count))
+        execute_command('cmake --build . -- -j' + str(multi_proc_count))
     else:
         log_info("Building in single core mode.")
-        os.system('make')
+        execute_command('cmake --build .')
     log_success("Build Complete.")
     print ""
     log_success("Add following lines to your .bashrc")
